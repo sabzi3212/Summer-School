e581c945cfe07cbf657c753abcb78f6a89a1849b
sabzi3212 <sabbirahmed.lu1@gmail.com>
2023-06-11 16:29:42 +0600

    auth and signup enabled

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from '../firebase/firebase.config';
+
 
 
 
 export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
 
 
 const AuthProvider = ({children}) => {
@@ -12,9 +17,53 @@ const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    
+    //create user
+
+    const createUser = (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    //signIn user
+
+    const signIn = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // signout
+
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    //statechange
+
+    useEffect(()=>{
+       const unsubscribe =  onAuthStateChanged(auth, currentUser=>{
+            setUser(currentUser);
+            console.log('current User', currentUser);
+            setLoading(false);
+        });
+
+        return () =>{
+
+            return unsubscribe();
+
+        }
+       
+
+    }, [])
+
+
     const authInfo = {
         user,
-        loading
+        loading,
+        createUser,
+        signIn,
+        logOut
 
     }
     return (
