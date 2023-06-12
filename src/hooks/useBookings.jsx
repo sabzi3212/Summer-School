02872f58d1f95cac2699907900d984c2956e3eea
sabzi3212 <sabbirahmed.lu1@gmail.com>
2023-06-12 18:25:45 +0600

    axios interceptor updated

--- src/hooks/useBookings.jsx
+++ src/hooks/useBookings.jsx
@@ -1,15 +1,27 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useBookings = () =>{
     const {user} = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
 
     const { isLoading, refetch, data: bookings = [] } = useQuery({
         queryKey: ['bookings', user?.email],
+        // queryFn: async ()=>{
+        //     const response = await fetch(`http://localhost:5000/bookings?email=${user?.email}`, {
+        //         headers: {
+        //             authorization : `bearer ${token}`
+        //         }
+        //     })
+        //     return response.json();
+        // },
         queryFn: async ()=>{
-            const response = await fetch(`http://localhost:5000/bookings?email=${user?.email}`)
-            return response.json();
+            const response = await axiosSecure(`/bookings?email=${user?.email}`)
+            console.log('res from axios', response)
+            return response.data;
         },
       })
 
